866a5c139cd0b6b2b1690844395953247e590d3d
Chris Smith <csmith@hashicorp.com>
2021-01-15 16:13:01 -0600

    documented output

--- output.tf
+++ output.tf
@@ -1,16 +1,20 @@
 
 output repo_desc {
+  description = "The description of the GitHub repo."
   value = github_repository.dev_repo.description
 }
 
 output repo_fullname {
+  description = "The full name of the GitHub repo."
   value = github_repository.dev_repo.full_name
 }
 
 output tfc_workspace_name {
+  description = "The name of the TFC workspace."
   value = github_repository.dev_repo.name
 }
 
 output repo_clone_url {
+  description = "The HTTP clone URL for the repo."
   value = github_repository.dev_repo.http_clone_url
 }
